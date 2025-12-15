04a7339bd801578359247e1ed2d1ebd3b479386f
Adam <adamgruda1312@gmail.com>
2025-12-15 17:47:51 +0100

    Controllers work, start from the main scene

--- src/world.gd
+++ src/world.gd
@@ -4,6 +4,8 @@ var interface : XRInterface
 
 func _ready() -> void:
 	interface = XRServer.find_interface("OpenXR")
+
 	if interface and interface.is_initialized():
+		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
 		get_viewport().use_xr = true
  
